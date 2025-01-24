8312a5ee8483775db5a171d74b6fdb825ffcf9b6
Victor Machado <victormachadogp@gmail.com>
2025-01-24 14:57:31 -0300

    feat(student): Add joi schema

---
+++ server/src/validations/student.validation.ts
@@ -0,0 +1,28 @@
+import Joi from "joi";
+
+export const studentSchema = Joi.object({
+  name: Joi.string().min(3).max(100).required().messages({
+    "string.min": "Nome deve ter no mínimo 3 caracteres",
+    "string.max": "Nome deve ter no máximo 100 caracteres",
+  }),
+
+  email: Joi.string().email().required().messages({
+    "string.email": "Email inválido",
+  }),
+
+  ra: Joi.string()
+    .pattern(/^[0-9]+$/)
+    .length(10)
+    .required()
+    .messages({
+      "string.pattern.base": "RA deve conter apenas números",
+      "string.length": "RA deve ter 10 dígitos",
+    }),
+
+  cpf: Joi.string()
+    .pattern(/^\d{11}$/)
+    .required()
+    .messages({
+      "string.pattern.base": "CPF inválido",
+    }),
+});
