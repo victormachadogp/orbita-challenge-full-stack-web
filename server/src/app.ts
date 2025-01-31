8894c811fc62f35e3223720285776d39e97a6343
Victor Machado <victormachadogp@gmail.com>
2025-01-31 00:41:34 -0300

    feat(server): Add cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
 import sequelize from "./config/database";
 import studentRoutes from "./routes/student.routes";
 import { errorHandler } from "./middlewares/errorHandler";
@@ -6,6 +7,13 @@ import { errorHandler } from "./middlewares/errorHandler";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(
+  cors({
+    origin: "http://localhost:5173", // URL do frontend Vue
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use(studentRoutes);
 
